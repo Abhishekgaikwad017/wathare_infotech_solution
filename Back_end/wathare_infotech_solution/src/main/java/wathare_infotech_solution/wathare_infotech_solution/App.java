fffb5d4303d494e3ec2a60ed78c1231fba3442f9
Abhishek Gaikwad <abhishekgaikwad0609@gmail.com>
2024-04-17 20:00:07 +0530

    REST API

---
+++ Back_end/wathare_infotech_solution/src/main/java/wathare_infotech_solution/wathare_infotech_solution/App.java
@@ -0,0 +1,13 @@
+package wathare_infotech_solution.wathare_infotech_solution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
